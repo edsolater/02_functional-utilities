2b1502f278027efd3a2f018c31238ef2ad8c050c
edsolater <edsolater@qq.com>
2020-09-13 19:20:06 +0800

    补上索引

---
+++ src/code_1/equal/areEqualObject.ts
@@ -0,0 +1,7 @@
+import { isObject } from '../judger/isObject'
+import { areEqualArray } from './areEqualArray'
+
+export const areEqualObject = (val1: object, val2: object) =>
+  isObject(val1) &&
+  isObject(val2) &&
+  areEqualArray(Object.entries(val1).flat(), Object.entries(val2).flat())
---
+++ src/equal.ts
@@ -0,0 +1,13 @@
+export { areSame } from './code_1/equal/areSame'
+export { areShallowEqual } from './code_1/equal/areShallowEqual'
+export { areEqual } from './code_1/equal/areEqual'
+export { areDeepEqual } from './code_1/equal/areDeepEqual'
+
+export { areEqualArray } from './code_1/equal/areEqualArray'
+export { areEqualObject } from './code_1/equal/areEqualObject'
+
+export { areLengthEqual } from './code_1/equal/areLengthEqual'
+export { areSizeEqual } from './code_1/equal/areSizeEqual'
+export { areKeyEqual } from './code_1/equal/areKeyEqual'
+export { areValueEqual } from './code_1/equal/areValueEqual'
+export { areEntriesEqual } from './code_1/equal/areEntriesEqual'
