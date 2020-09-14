d6522e42299391fb43f3398b6da00562b84c196e
edsolater <edsolater@qq.com>
2020-09-14 20:26:53 +0800

    partialCall 标记为deprecated，因为与JS的bind功能相同

--- src/code_2/funcUtil/partialCall.test.ts
+++ src/code_2/funcUtil/partialCall.test.ts
@@ -2,7 +2,9 @@ import { partialCall } from './partialCall'
 
 const add = (a: number, b: string) => a + b
 const testFn = partialCall(add, 3)
+const testFn2 = add.bind(undefined, 3)
 
 test('基础案例', () => {
   expect(testFn('1')).toBe(add(3, '1'))
+  expect(testFn2('1')).toBe(add(3, '1'))
 })
--- src/code_2/funcUtil/partialCall.ts
+++ src/code_2/funcUtil/partialCall.ts
@@ -1,5 +1,9 @@
 type Arr = readonly unknown[]
-
+/**
+ * @deprecated 这不就是bind吗？
+ * @param fn 
+ * @param headArgs 
+ */
 export const partialCall = <T extends Arr, U extends Arr, R>(
   fn: (...args: [...T, ...U]) => R,
   ...headArgs: T
