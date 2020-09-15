7e200a9c2349d1a9b7e5f8ab790ca1e4014383a3
zhanggongying <edsolater@qq.com>
2020-09-15 18:03:27 +0800

    没想到，去除index文件异常的顺利

--- src/codeFiles/pickItem/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as getFirst } from './getFirst'
-export { default as getLast } from './getLast'
-export { default as getProperty } from './getProperty'
--- src/codeFiles/toArray/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as tail } from './tail'
