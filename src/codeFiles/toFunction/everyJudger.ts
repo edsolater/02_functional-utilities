2ca801b2e2d6512ded837b1b1f07190e20a632bb
zhanggongying <edsolater@qq.com>
2020-09-15 20:12:44 +0800

    占位方法pipe与everyJudger（这名字起得不好）

---
+++ src/codeFiles/toFunction/everyJudger.ts
@@ -0,0 +1,6 @@
+/**
+ * 当每个judger都满足时返回true，否则，返回false
+ * @todo
+ */
+const everyJudger = () => true
+export default everyJudger
---
+++ src/codeFiles/toFunction/pipe.ts
@@ -0,0 +1,6 @@
+// TODO 这是一个很重要发方法
+/**
+ * @todo
+ */
+const pipe = () => true
+export default pipe
