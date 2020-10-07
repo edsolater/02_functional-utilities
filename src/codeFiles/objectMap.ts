645739e161d81c231d1aa3970c7d277f3a247820
edsolater <edsolater@qq.com>
2020-10-07 23:38:39 +0800

    加入objectMap

---
+++ src/codeFiles/objectMap.ts
@@ -0,0 +1,19 @@
+/**
+ * 纯函数
+ *
+ * 基于对象的每一个value，映射， 返回新的对象
+ * 该对象有相同的属性数量
+ *
+ * 类似于array.protoype.map
+ * @param target 目标对象
+ * @param mapperFn 映射函数
+ */
+const objectMap = <T extends object, U>(
+  target: T,
+  mapperFn: (entry: [key: keyof T, value: T[keyof T]], index: number) => [key: string, value: U]
+): { [key: string]: U } => {
+  //@ts-ignore
+  return Object.fromEntries(Object.entries(target).map(mapperFn))
+}
+
+export default objectMap
